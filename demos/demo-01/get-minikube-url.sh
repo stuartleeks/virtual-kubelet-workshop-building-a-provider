36ca2d815a8b921751fefa9a126029be5c895c15
Stuart Leeks <stuartle@microsoft.com>
2018-08-28 19:56:08 +0100

    Add instructions for connecting to UI to helm chart

--- demos/demo-01/get-minikube-url.sh
+++
@@ -1,4 +0,0 @@
-uiPort=$(kubectl get service vk-demo-01 -o jsonpath={.spec.ports[0].nodePort}) && \
-minikubeIp=$(minikube ip) && \
-echo "The web UI is listening on  http://$minikubeIp:${uiPort}"
-echo "The web API is listening on http://$minikubeIp:${uiPort}/api/"
